aaf1a373859aace1e465c22abb93e354eca349e4
Zachary Sirna <zsirna@yahoo.com>
2022-02-28 13:44:38 -0500

    commit tree 69de509b9af66ad57fba8030c67fe41122dfe904

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, message) {
+    let newArray = []
+    for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+  }
+  return newArray
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number--);
+      }
+}
